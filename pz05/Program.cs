a1b298914f26814401b4e41df2eee0db83918251
Юлия Акуленкова <akulenok2006@mail.ru>
2023-09-24 22:56:40 -1200

    добавила 5 практику

---
+++ pz05/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace pz05
+ class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int i = 1;
+        while (i * i <= n)
+        {
+            Console.Write(i * i + " ");
+            i++;
+        }
+    }
+}
