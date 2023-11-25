5e07adc1fa16d77cb207b757db4b1b110e3c5553
Юлия Акуленкова <akulenok2006@mail.ru>
2023-11-24 14:47:06 -1200

    добавила 10 практику

---
+++ pz_10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Это первое предложение. Это второе предложение!";
+
+            int firstPeriodIndex = text.IndexOf('.');
+            int lastPeriodIndex = text.LastIndexOf('.');
+
+            if (firstPeriodIndex != -1 && lastPeriodIndex != -1)
+            {
+                string firstSentence = text.Substring(0, firstPeriodIndex + 1);
+                string lastSentence = text.Substring(lastPeriodIndex + 1);
+                string middleText = text.Substring(firstPeriodIndex + 1, lastPeriodIndex - firstPeriodIndex);
+
+                string newText = lastSentence + middleText + firstSentence;
+                Console.WriteLine(newText);
+            }
+            else
+            {
+                Console.WriteLine("Текст не содержит предложений.");
+            }
+        }
+    }
+}
+    
+
