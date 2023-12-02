4b9ac871ca76da7b7ecffe552a14ecf35877f963
Юлия Акуленкова <akulenok2006@mail.ru>
2023-12-01 15:41:18 -1200

    добавила 12 практику

---
+++ pz_12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.Design;
+
+namespace pz_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int num = Convert.ToInt32(Console.ReadLine());
+          string[] massive = PrintArray(num);
+          foreach(string i in massive)
+          {
+                Console.WriteLine(i);
+          }
+        }
+        static string[] PrintArray(int a)
+        {
+            string[]nums = new string[a];
+            string stroka = "";
+            for(int i = 0; i < a; i++)
+            {
+                if ((i + 1) % 2 == 0)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        stroka += "*";
+                    }
+                }
+                else if ((i + 1) % 2 != 0)
+                {
+                    for(int j = 0;j <= i; j++)
+                    {
+                        if (j == (i + 1) / 2 && j != 0)
+                        {
+                            stroka += ".";
+                        }
+                    }
+                }
+                nums[i] = stroka;
+                stroka = "";
+            } 
+            return nums;
+            
+        }
+    }
+}
