208b8480449f9775b59620d21674fc2d9128c00a
Юлия Акуленкова <akulenok2006@mail.ru>
2023-12-08 08:58:46 -1200

    добавила 15 практику

---
+++ pz_15/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace pz_15;
+using System;
+using System.IO;
+
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            string directoryPath = "C:\\Program Files";
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+
+            Console.WriteLine("Listing directories in " + directoryPath + " sorted by decreasing length:");
+
+            DirectoryInfo[] directories = directory.GetDirectories();
+            Array.Sort(directories, (x, y) => y.Name.Length.CompareTo(x.Name.Length));
+
+            foreach (DirectoryInfo dir in directories)
+            {
+                Console.WriteLine(dir.Name);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+
+}
