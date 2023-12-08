cc7ce5ee323952527b0bb8dbe3e8a3299ca610b8
Юлия Акуленкова <akulenok2006@mail.ru>
2023-12-07 13:24:56 -1200

    добавила 14 практику

---
+++ pz_14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Введите строку для поиска:");
+            string searchString = Console.ReadLine();
+
+            string filePath = "C:\\Users\\Admin\\source\\repos\\pz_14\\pz_14/textFile.txt"; // замените на путь к вашему файлу
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains(searchString))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
